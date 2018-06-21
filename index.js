5fcc5030bae3e889e7040eec4f9fde565cf6f340
Sergey Sova <mail@sergeysova.com>
2018-06-21 03:46:22 +0300

    Revert "feat(just return FSA compliant):"
    This reverts commit c21a06d8882abd64c49371a5073980f1e974d3b5.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 'use strict'
 
+function getSymbolCreator() {
+  return typeof Symbol === 'function'
+    ? Symbol
+    : function Symbol(name) { return '@@redux-symbiote/' + name };
+}
+
+var createSymbol = getSymbolCreator()
 
 module.exports = {
-  action: '@@redux-symbiote/action',
+  getActionCreator: createSymbol('action function for actions list'),
 };
