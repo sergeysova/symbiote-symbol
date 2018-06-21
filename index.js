c21a06d8882abd64c49371a5073980f1e974d3b5
Sergey Sova <mail@sergeysova.com>
2018-06-21 03:32:23 +0300

    feat(just return FSA compliant):

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 'use strict'
 
-function getSymbolCreator() {
-  return typeof Symbol === 'function'
-    ? Symbol
-    : function Symbol(name) { return '@@redux-symbiote/' + name };
-}
-
-var createSymbol = getSymbolCreator()
 
 module.exports = {
-  getActionCreator: createSymbol('action function for actions list'),
+  action: '@@redux-symbiote/action',
 };
